11208a63f05bb795f12e70209d36bc0f6fd5f7a5
ibsenc <ibsen.camille@gmail.com>
2023-04-27 13:59:56 -0700

    Implemented APIs for logout, register, and logged in user

--- backend/apis/users.js
+++ backend/apis/users.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router();
 const UsersModel = require('../db/users/users.model');
 // const bcrypt = require("bcryptjs");
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 // /api/users/
+
 router.get('/', async function(request, response) {
     try {
         const allUsersResponse = await UsersModel.getAllUsers()
@@ -12,6 +13,32 @@ router.get('/', async function(request, response) {
     } catch (error) {
         console.error(error)
         return response.status(500).send(error)
+    }
+});
+
+router.get('/loggedinuser', async function(request, response) {
+    const jwtPayload = request.cookies.userToken;
+
+    if(!jwtPayload) {
+        console.info("No user is logged in.")
+        return response.send({userId: null, username: null})
+    }
+
+    let decryptedJwtPayload;
+    try {
+        decryptedJwtPayload = jwt.verify(jwtPayload, "Camille's password");
+    } catch(e) {
+        console.error(e);
+        return response.send({userId: null, username: null})
+    }
+
+    const {userId, username} = decryptedJwtPayload;
+
+    if(!userId || !username) {
+        console.info(`UserId ${userId} or username ${username} is null.`)
+        return response.send({userId: null, username: null})
+    } else {
+        return response.send({userId: userId, username: username})
     }
 });
 
@@ -39,18 +66,29 @@ router.get('/username/:username', async function(request, response) {
     }
 });
 
-router.post('/', async function(request, response) {
+router.post('/register', async function(request, response) {
 
     if(!request.body.username || !request.body.password) {
-        return res.status(404).send({message: "Must include username AND password"});
+        return res.status(400).send({message: "Must include username AND password"});
     }
 
-    request.body.password = bcrypt.hashSync(request.body.password, 10);
+    // request.body.password = bcrypt.hashSync(request.body.password, 10);
 
     const newUser = request.body;
 
     try {
         const createUserResponse = await UsersModel.createUser(newUser)
+        const userId = createUserResponse._id;
+        const username = createUserResponse.username;
+
+        const jwtPayload = {
+            userId,
+            username
+        }
+
+        const token = jwt.sign(jwtPayload, "Camille's password")
+        response.cookie("userToken", token);
+
         return response.send(createUserResponse)
     } catch (error) {
         console.error(error)
@@ -78,9 +116,13 @@ router.delete('/:userId', async function(request, response) {
     return response.send("Successfully deleted user")
 });
 
-router.post('/login', async function(request, res) {
+router.post('/login', async function(request, response) {
     const username = request.body.username;
     let password = request.body.password;
+
+    if(!username || !password) {
+        return res.status(400).send({message: "Must include username AND password"});
+    }
 
     try {
         const getUserResponse = await UsersModel.getUserByUsername(username);
@@ -99,7 +141,7 @@ router.post('/login', async function(request, res) {
         // }
 
         if (getUserResponse.password !== password) {
-            return res.status(403).send("Username or password is incorrect.");
+            return response.status(403).send("Username or password is incorrect.");
         }
 
         const userId = getUserResponse.userId;
@@ -110,15 +152,25 @@ router.post('/login', async function(request, res) {
         }
 
         const token = jwt.sign(jwtPayload, "Camille's password")
-
-        res.cookie("userToken", token);
+        response.cookie("userToken", token);
         
-        return res.send("Logged in successfully");
+        return response.send("Logged in successfully");
     
     } catch (e) {
         console.error(e);
-        res.status(401).send("Unauthorized: access denied.");
+        response.status(401).send("Unauthorized: access denied.");
     }
-})
+});
+
+router.post('/logout', async function(request, response) {
+
+    response.cookie('userToken', {}, {
+        maxAge: 0,
+    })
+
+    response.send(true);
+});
+
+
 
 module.exports = router;
